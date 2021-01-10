1686b07c64a8d456c6b27726b6752d972bdc60ab
马达 <mada365@gmail.com>
2021-01-10 22:13:35 +0800

    新建飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,18 @@
 import sys
 import pygame
+from settings import Settings
 def run_game():
     # 初始化创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_hight)
+    )
     # 窗口的标题
     pygame.display.set_caption("Alien Invasion")
     # 开始游戏主循环
     while True:
+        screen.fill(ai_settings.bg_color)
         # 监控键盘和鼠标
         for event in pygame.event.get():
             # 玩家点击quit
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    '''储存游戏的所有设置的类'''
+    def __init__(self):
+        '''初始化游戏设置'''
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_hight = 800
+        self.bg_color = (230,230,230)
