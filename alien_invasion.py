0063a46a39bf6ba0c3295219612c5b2aba6830f1
马达 <mada365@gmail.com>
2021-01-10 21:41:06 +0800

    吊带

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,14 +4,16 @@ def run_game():
     # 初始化创建一个屏幕对象
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
+    # 窗口的标题
     pygame.display.set_caption("Alien Invasion")
     # 开始游戏主循环
     while True:
         # 监控键盘和鼠标
         for event in pygame.event.get():
+            # 玩家点击quit
            if event.type == pygame.QUIT:
                sys.exit()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
-        run_game()
+run_game()
