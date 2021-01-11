94b7a22f91f551f9a6c4bce60f9bbe8ab2c702b2
马达 <mada365@gmail.com>
2021-01-12 05:46:54 +0800

    新建飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,30 @@
-import sys
+
 import pygame
 from settings import Settings
+from ship import  Ship
+import game_functions as gf
 def run_game():
     # 初始化创建一个屏幕对象
     pygame.init()
     ai_settings = Settings()
+    # 绘制一个屏幕
     screen = pygame.display.set_mode(
         (ai_settings.screen_width,ai_settings.screen_hight)
     )
     # 窗口的标题
     pygame.display.set_caption("Alien Invasion")
+
+    #创建一艘飞船
+    ship = Ship(screen)
     # 开始游戏主循环
     while True:
-        screen.fill(ai_settings.bg_color)
+        # 将飞船绘制在屏幕上
+        ship.blitme()
         # 监控键盘和鼠标
-        for event in pygame.event.get():
-            # 玩家点击quit
-           if event.type == pygame.QUIT:
-               sys.exit()
-
+        gf.check_events()
+        # 每次循环都重绘屏幕
+        screen.fill(ai_settings.bg_color)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
+
 run_game()
--- ship.py
+++ ship.py
@@ -9,11 +9,13 @@ class Ship():
 
         # 加载飞船图像并获取外形矩形
         self.image = pygame.image.load('images/ship.bmp')
+        # 获取飞船的n多种属性
         self.rect = self.image.get_rect()
+        #获取窗口的n多种属性
         self.screen_rect = screen.get_rect()
-        # 将新飞船放在屏幕底部中央
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        # 将窗口的中间、底部坐标属性复制给飞船
+        self.rect.centerx = self.screen_rect.centerx  #中间
+        self.rect.bottom = self.screen_rect.bottom   #底部
 
     def blitme(self):
         """在指定位置绘制飞船"""
