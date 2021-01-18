302509a8a37e6135ded38bccd23719f136c0b35d
mada <mada365@outlook.com>
2021-01-18 19:12:59 +0800

    外星人干饭

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,7 +24,7 @@ def run_game():
     #外星人编组
     aliens = Group()
     # 创建外星人群
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen,ship, aliens)
 
     # 开始游戏主循环
     while True:
