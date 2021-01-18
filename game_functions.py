8a9fdf5be8aabae78233ff2c1ff76e89ae58b953
mada <mada365@outlook.com>
2021-01-18 19:26:00 +0800

    外星人干饭

--- game_functions.py
+++ game_functions.py
@@ -95,13 +95,13 @@ def create_fleet(ai_settings,screen,ship, aliens):
     number_aliens_x = get_number_aliens_x(ai_settings,alien.rect.width)
     number_rows = get_number_rows(ai_settings,ship.rect.height,alien.rect.height)
     #创建第一行外星人
-    for alien_number in range(number_aliens_x):
-        create_alien(ai_settings, screen, aliens, alien_number)
+    # for alien_number in range(number_aliens_x):
+    #     create_alien(ai_settings, screen, aliens, alien_number)
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings,screen,aliens,alien_number,row_number)
 def get_number_rows(ai_settings,ship_height,alien_height):
     """计算屏幕可以容纳多少外星人"""
-    available_space_y = (ai_settings.screen_height - (3*alien_height) - ship_height)
+    available_space_y = (ai_settings.screen_hight - (3*alien_height) - ship_height)
     number_rows = int(available_space_y/(2*alien_height))
     return number_rows
